c7c82a522a1ab675a1fcc875b0a1e99da7525bcf
FarisAmri <fofexm@gmail.com>
2025-02-01 21:02:07 +0300

    updated sql

--- restaurantdb.sql
+++ restaurantdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 18, 2025 at 02:26 PM
+-- Generation Time: Feb 01, 2025 at 07:00 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,7 +40,8 @@ CREATE TABLE `customers` (
 
 INSERT INTO `customers` (`customer_id`, `name`, `phone`, `email`) VALUES
 (1, 'John Doe', '1234567890', 'john.doe@example.com'),
-(2, 'Jane Smith', '0987654321', 'jane.smith@example.com');
+(2, 'Jane Smith', '0987654321', 'jane.smith@example.com'),
+(3, 'Faris', '123456789', 'faris@example.com');
 
 -- --------------------------------------------------------
 
@@ -61,7 +62,8 @@ CREATE TABLE `reservations` (
 --
 
 INSERT INTO `reservations` (`reservation_id`, `table_id`, `customer_id`, `reservation_time`, `duration`) VALUES
-(1, 1, 1, '2025-01-18 19:00:00', 90);
+(13, 6, 3, '2025-01-01 01:00:00', 1),
+(14, 6, 3, '2025-01-01 01:02:00', 5);
 
 -- --------------------------------------------------------
 
@@ -82,10 +84,9 @@ CREATE TABLE `tables` (
 --
 
 INSERT INTO `tables` (`table_id`, `table_number`, `capacity`, `location`, `status`) VALUES
-(1, 'T01', 4, 'Window', 'available'),
-(5, '5', 3, 'right', 'available'),
-(6, '2', 2, 'lower', 'available'),
-(7, '105', 5, 'corner', 'available');
+(6, '2', 2, 'lower', 'reserved'),
+(7, '8', 5, 'corner', 'available'),
+(11, '3', 2, 'lower', 'available');
 
 --
 -- Indexes for dumped tables
@@ -119,19 +120,19 @@ ALTER TABLE `tables`
 -- AUTO_INCREMENT for table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `tables`
 --
 ALTER TABLE `tables`
-  MODIFY `table_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `table_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- Constraints for dumped tables
